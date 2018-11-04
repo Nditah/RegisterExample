6cb74a427d45a17a6e5d92043e3bbd1a4eb1c484
Leo Wang <leuowang@gmail.com>
2018-11-04 11:23:32 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBz6mvrJpytHpTbVmrsKPclOSqHIIZi-TM",
-    authDomain: "canai-crm.firebaseapp.com",
-    databaseURL: "https://canai-crm.firebaseio.com",
-    projectId: "canai-crm",
-    storageBucket: "canai-crm.appspot.com",
-    messagingSenderId: "562836142713"
+    /* apikey ... */
   }
 };
